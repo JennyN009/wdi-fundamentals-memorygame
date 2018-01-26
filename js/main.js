8b2cc02e2b3d68d3ae9bf7096e4d46d40340fa1b
Jenny Narvas <jenny.g.narvas@disney.com>
2018-01-25 20:21:47 -0800

    UNIT 11 assignment memory game

--- js/main.js
+++ js/main.js
@@ -21,15 +21,22 @@ var cards = [
   }
 
 ];
+
 var cardsInPlay = [];
+
+//scores wrong and right answers
+var x = 0;
+var y = 0;
 
 var checkForMatch = function(){
 
   if (cardsInPlay[0] === cardsInPlay[1]) {
     alert("You found a match!");
-  }
+    x = x + 1;
+      }
   else {
     alert("Sorry, try again.");
+    y = y + 1;
   }
 
 }
@@ -37,16 +44,61 @@ var checkForMatch = function(){
 
 var flipCard = function(cardId){
 
-      console.log("User flipped " + cards[cardId].rank);
-      cardsInPlay.push(cards[cardId].rank);
+      var cardId = this.getAttribute('data-id');
 
-      console.log(cards[cardId].cardImage);
-      console.log(cards[cardId].suit);
+
+    //  console.log("User flipped " + cards[cardId].rank);
+     cardsInPlay.push(cards[cardId].rank);
+
+    //  console.log(cards[cardId].cardImage);
+     //console.log(cards[cardId].suit);
+
+
+      this.setAttribute('src', cards[cardId].cardImage);
 
       if (cardsInPlay.length === 2){
           checkForMatch();
             }
 }
 
-flipCard(0);
-flipCard(2);
+var createBoard = function(){
+
+  for (var i = 0; i < cards.length; i++){
+    var cardElement = document.createElement('img');
+        cardElement.setAttribute('src', 'images/back.png');
+        cardElement.setAttribute('data-id', i);
+        cardElement.addEventListener('click', flipCard);
+        document.getElementById('game-board').appendChild(cardElement);
+
+  }
+}
+
+var resetGame = function(){
+      for (var i = 0; i < 4; i++){
+        var cardsFlipped = document.getElementById('game-board');
+             cardsFlipped.removeChild(cardsFlipped.childNodes[0]);
+             cardsInPlay.pop();
+      }
+
+createBoard();
+}
+
+
+var totalScore = function(){
+    alert('You got ' + x + ' right and you got ' + y + ' wrong.');
+
+}
+
+
+//Bonus
+var myButton = document.getElementById("clicker");
+
+    myButton.addEventListener('click', resetGame);
+
+//challenge
+var getScore = document.getElementById("score");
+
+    getScore.addEventListener('click', totalScore);
+
+
+createBoard();
